5acfbc21868733f6c3e7709d20bcd8f326faf0d6
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-21 13:05:21 -0300

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,34 @@
+import { initializeApp, getApps, getApp } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-storage.js';
+
+import firebase from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js';
+import 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore-compat.js';
+import 'https://www.gstatic.com/firebasejs/9.22.2/firebase-auth-compat.js';
+import 'https://www.gstatic.com/firebasejs/9.22.2/firebase-storage-compat.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC78l9b2DTNj64y_0fbRKofNupO6NHDmeo",
+  authDomain: "matheus-35023.firebaseapp.com",
+  projectId: "matheus-35023",
+  storageBucket: "matheus-35023.appspot.com",
+  messagingSenderId: "1011113149395",
+  appId: "1:1011113149395:web:c1f449e0e974ca8ecb2526"
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+window.firebase = firebase;
+window.auth = auth;
+window.db = db;
+window.storage = storage;
+
+export { app, auth, db, storage };
