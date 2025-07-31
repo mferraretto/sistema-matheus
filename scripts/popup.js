c2afd1559e3a231b6f4b41ce84a1086e17a0f6a9
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-31 11:34:26 -0300

    Create popup.js

---
+++ scripts/popup.js
@@ -0,0 +1,7 @@
+document.getElementById("syncBtn").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+files: ["scripts/content.js"]
+  });
+});
