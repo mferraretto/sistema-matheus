1c7dbf36c33e6eaadbff7d9224010cf2b89bb7b8
mferraretto <matheus.ferraretto94@gmail.com>
2025-08-03 11:31:03 -0300

    Update expose-uid.js

--- expose-uid.js
+++ expose-uid.js
@@ -2,7 +2,7 @@
 window.addEventListener("message", (event) => {
   if (event.data === "getShopeeUser") {
     const uid = window.sistema?.uid || null;
-    const passphrase = sessionStorage.getItem("senhaCripto") || null;
+    const passphrase = typeof getPassphrase === 'function' ? getPassphrase() : null;
 
     event.source.postMessage({
       type: "shopeeUserInfo",
