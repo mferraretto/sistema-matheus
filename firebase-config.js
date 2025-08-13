d64c8580e9623dd762a8c73e5d4720026f71a1b0
mferraretto <matheus.ferraretto94@gmail.com>
2025-08-13 09:44:02 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ export const firebaseConfig = {
       apiKey: "AIzaSyC78l9b2DTNj64y_0fbRKofNupO6NHDmeo",
       authDomain: "matheus-35023.firebaseapp.com",
       projectId: "matheus-35023",
-      storageBucket: "matheus-35023.appspot.com",
+      storageBucket: "matheus-35023.firebasestorage.app",
       messagingSenderId: "1011113149395",
       appId: "1:1011113149395:web:c1f449e0e974ca8ecb2526",
       databaseURL: "https://matheus-35023.firebaseio.com"
