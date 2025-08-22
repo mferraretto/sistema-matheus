57eecf8970299037731e882afc58d36de1a1996c
mferraretto <matheus.ferraretto94@gmail.com>
2025-08-22 10:10:18 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'financeiro-cache-v1';
+const URLS_TO_CACHE = [
+  '/financeiro.html',
+  '/financeiro.js',
+  '/css/styles.css',
+  '/css/components.css',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
