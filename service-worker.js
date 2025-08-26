c776efb67d05323cca5a24d1502803bb60a35d5c
mferraretto <matheus.ferraretto94@gmail.com>
2025-08-26 16:40:43 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,28 @@
-const CACHE_NAME = 'financeiro-cache-v1';
+const CACHE_VERSION = '20240826';
+const CACHE_PREFIX  = 'financeiro-cache-v';
+const CACHE_NAME    = `${CACHE_PREFIX}${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   'financeiro.html',
   'financeiro.js',
-  'css/styles.css',
+  `css/styles.css?v=${CACHE_VERSION}`,
   'css/components.css',
   'icons/icon-192.png',
-  'icons/icon-512.png'
+  'icons/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
   );
 });
 
