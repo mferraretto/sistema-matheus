86c62686fb2a6a48721e226266d18024f233ccf3
mferraretto <matheus.ferraretto94@gmail.com>
2025-09-11 20:50:19 -0300

    fix: apply sidebar styles globally

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,9 @@ const URLS_TO_CACHE = [
 
 // Workbox handles the installation and activation steps automatically.
 // The precaching list is used by Workbox's precaching module.
-workbox.precaching.precacheAndRoute(URLS_TO_CACHE.map(url => ({ url, revision: null })));
+workbox.precaching.precacheAndRoute(
+  URLS_TO_CACHE.map((url) => ({ url, revision: null })),
+);
 
 // A simple routing example: use a CacheFirst strategy for images.
 workbox.routing.registerRoute(
@@ -33,7 +35,8 @@ workbox.routing.registerRoute(
 
 // Another example: a StaleWhileRevalidate strategy for CSS and JS.
 workbox.routing.registerRoute(
-  ({ request }) => request.destination === 'script' || request.destination === 'style',
+  ({ request }) =>
+    request.destination === 'script' || request.destination === 'style',
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'assets-cache',
   }),
