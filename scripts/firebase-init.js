8b4288d0e4daa7c918475f0bfb7a5a938d3beb3b
mferraretto <matheus.ferraretto94@gmail.com>
2025-09-12 08:16:55 -0300

    Revert "refactor: centralize firebase initialization"

---
+++ scripts/firebase-init.js
@@ -0,0 +1,24 @@
+import { initializeApp, getApps } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js';
+import {
+  getFirestore,
+  collection,
+  doc,
+  setDoc
+} from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
+
+// Configuração Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC78l9b2DTNj64y_0fbRKofNupO6NHDmeo",
+  authDomain: "matheus-35023.firebaseapp.com",
+  projectId: "matheus-35023",
+  storageBucket: "matheus-35023.appspot.com",
+  messagingSenderId: "1011113149395",
+  appId: "1:1011113149395:web:c1f449e0e974ca8ecb2526"
+};
+
+// Inicializa Firebase
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// 🔁 Exporte tudo que o background precisa:
+export { db, collection, doc, setDoc };
